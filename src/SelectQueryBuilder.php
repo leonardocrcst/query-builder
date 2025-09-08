90e8b29c10898ecb5e38cbf0848b4b9e36e8a706
Leonardo Cruz Costa <88503342+leonardocrcst@users.noreply.github.com>
2025-09-08 17:06:43 -0300

    Refactor column handling in `SelectQueryBuilder` and add support for SQL joins
    **Objectives**:
    - Replace direct `columns` property assignment with `addColumn` method for improved flexibility and alias support.
    - Introduce support for SQL joins in `SelectQueryBuilder` via `setJoin` method to add `JOIN` clauses with conditions.
    - Modify `__toString` to include join statements in the generated SQL query.
    - Update unit tests to validate join functionality and new column handling approach.
    - Ensure backward compatibility and extendability for future enhancements in query building.

--- src/SelectQueryBuilder.php
+++ src/SelectQueryBuilder.php
@@ -2,22 +2,34 @@
 
 namespace Leonardocrcst\QueryBuilder;
 
+use Leonardocrcst\QueryBuilder\Clausules\JoinClausule;
 use Leonardocrcst\QueryBuilder\Clausules\LimitClausule;
 use Leonardocrcst\QueryBuilder\Clausules\OrderClausule;
 use Leonardocrcst\QueryBuilder\Clausules\WhereClausule;
+use Leonardocrcst\QueryBuilder\Types\JoinType;
 
 class SelectQueryBuilder
 {
     protected OrderClausule $order;
     protected LimitClausule $limit;
     protected WhereClausule $where;
+    protected array $joins = [];
+    protected array $columns = [];
+    protected array $columnsAlias = [];
 
-    public array $columns {
-        set => $this->columns = $value;
-    }
     protected string $getColumns {
-        get => !empty($this->columns) ? implode(', ', $this->columns) : '*';
+        get {
+            if (empty($this->columns)) {
+                return '*';
+            }
+            $columns = [];
+            foreach ($this->columns as $column) {
+                $columns[] = isset($this->columnsAlias[$column]) ? "$column AS " . $this->columnsAlias[$column] : $column;
+            }
+            return implode(', ', $columns);
+        }
     }
+
     protected string $getFrom {
         get => "FROM $this->table";
     }
@@ -30,11 +42,24 @@ class SelectQueryBuilder
         $this->where = new WhereClausule();
     }
 
+    public function setJoin(JoinType $joinType, string $joinTable): JoinClausule
+    {
+        $join = new JoinClausule($joinType, $this->table,$joinTable);
+        $this->joins[] = $join;
+        return $join;
+    }
+
+    public function addColumn(string $column, ?string $alias = null): self
+    {
+        $this->columns[] = $column;
+        if (!empty($alias)) {
+            $this->columnsAlias[$column] = $alias;
+        }
+        return $this;
+    }
+
     public function setOrder(string $term, string $direction = 'asc'): OrderClausule
     {
-        if (empty($this->order)) {
-            $this->order = new OrderClausule();
-        }
         $this->order->add($term, $direction);
         return $this->order;
     }
@@ -54,12 +79,18 @@ class SelectQueryBuilder
     public function __toString(): string
     {
         return trim(sprintf(
-            "SELECT %s %s %s %s %s",
+            "SELECT %s %s %s %s %s %s",
             $this->getColumns,
             $this->getFrom,
+            !empty($this->joins) ? implode(' ', $this->getJoins()) : null,
             $this->where->getWhere,
             $this->order->getOrders,
             $this->limit->getLimit
         ));
+    }
+
+    private function getJoins(): array
+    {
+        return array_map(fn(JoinClausule $join) => $join->getJoin, $this->joins);
     }
 }
